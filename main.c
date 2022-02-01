9dc9a9a8a41804d0d221bd03834293d7ec4da09d
sandemat <sandemat@oregonstate.edu>
2022-01-31 17:36:07 -0700

    fixed exit functionality and added flush statements for every potential print statement

--- main.c
+++ main.c
@@ -22,7 +22,6 @@ int main(void)
 
 	// Buffer to hold the user input
 	char* usrInput = malloc(sizeof(char));
-	int test;
 
 	// Variable to hold a new command struct
 	//struct command* userCommand;
@@ -30,38 +29,42 @@ int main(void)
 	do
 	{
 		// Send ':' to terminal to serve as the command prompt and check for error
-		//output = write(1, ": ", 2);
 		if (write(1, ": ", 2) == -1)
 		{
 			printf("Error output");
+			fflush(stdout);
 			exit(1);
 		}
-		fflush(stdout);  // Sends data in stdout to the screen 
 
 		usrInput = calloc(2049, sizeof(char));
+
 		// Read user input from terminal and check for error
-		//input = read(0, &inputBuff, 2048);
 		if (read(0, usrInput, 2048) == -1)
 		{
 			printf("Error read");
+			fflush(stdout);
 			exit(1);
 		}
 		
-	} while (strcmp(usrInput, "exit") != 0);  // End the input loop only if the user prompt has an error
+
+	} while (strcmp(usrInput, "exit\n") != 0);  // End the input loop only if the user prompt has an error
 
 	// Close stdin and stdout file descriptors before exiting program
 	if (close(0) == -1)
 	{
 		printf("Error close stdin");
+		fflush(stdout);
 		exit(1);
 	}
 
 	if (close(1) == -1)
 	{
 		printf("Error clost stdout");
+		fflush(stdout);
 		exit(1);
 	}
-
+	
+	free(usrInput);  // Free the memory allocated for input
 	exit(0);
 }
 
